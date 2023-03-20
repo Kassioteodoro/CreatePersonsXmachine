3829f5c217cf69f97c0b37d76f8d3b1252ace5cd
kassio <kassioteadoro@gmail.com>
2023-03-20 19:27:43 -0300

    feat: create server app

--- backend/src/appMongoDB.js
+++ backend/src/appMongoDB.js
@@ -3,4 +3,4 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-export default app;
+module.exports = app;
--- backend/src/database/MongoDB/Connection.js
+++ backend/src/database/MongoDB/Connection.js
@@ -2,4 +2,4 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = () => mongoose.connect('mongodb://localhost:27017/PersonDB');
 
-export default connectToDatabase;
+module.exports = connectToDatabase;
