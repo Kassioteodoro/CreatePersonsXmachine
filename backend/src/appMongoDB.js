abb2b8b816b956219f7f1d6e0655238ee3c58230
kassio <kassioteadoro@gmail.com>
2023-03-21 10:39:20 -0300

    feat: create PersonRoutes

--- backend/src/appMongoDB.js
+++ backend/src/appMongoDB.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
+const PersonRoute = require('./routes/personRoute');
+
 const app = express();
 app.use(express.json());
+app.use(PersonRoute);
 
 module.exports = app;
