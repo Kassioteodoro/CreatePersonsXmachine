9dd0f3e098543d4d4a5734b02aba7cfa1b0e19b2
kassio <kassioteadoro@gmail.com>
2023-03-28 15:56:07 -0300

    feat: Person service

--- backend/src/services/personService.js
+++ backend/src/services/personService.js
@@ -10,6 +10,50 @@ const create = async (person) => {
   }
 };
 
+const getAll = async () => {
+  try {
+    const response = await Person.find();
+    return { type: null, message: response };
+  } catch (err) {
+    console.log(err);
+    return { type: 500, message: err.message };
+  }
+};
+
+const getOne = async (person) => {
+  try {
+    const response = await Person.findById(person);
+    return { type: null, message: response };
+  } catch (err) {
+    console.log(err);
+    return { type: 500, message: err.message };
+  }
+};
+
+const update = async (person, id) => {
+  try {
+    await Person.updateOne(id, person);
+    return { type: null, message: 'update sucess' };
+  } catch (err) {
+    console.log(err);
+    return { type: 500, message: err.message };
+  }
+};
+
+const deleted = async (id) => {
+  try {
+    await Person.deleteOne(id);
+    return { type: null, message: 'remove sucess' };
+  } catch (err) {
+    console.log(err);
+    return { type: 500, message: err.message };
+  }
+};
+
 module.exports = {
   create,
+  getAll,
+  getOne,
+  update,
+  deleted,
 };
