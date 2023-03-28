abb4fd360643ff8234a63afdff094a831e147760
kassio <kassioteadoro@gmail.com>
2023-03-28 11:31:50 -0300

    feat: models, migrations, seeders MySQL

---
+++ backend/src/database/MySQLDB/migrations/20230328141627-User.js
@@ -0,0 +1,25 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('users');
+  },
+};
