f7972a069db3e514af41feebc3f049f52d867052
kassio <kassioteadoro@gmail.com>
2023-03-30 17:42:32 -0300

    build: init frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,61 +1,20 @@
-import axios from 'axios';
-import React, {useState} from 'react';
-// import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Login from './pages/Login'
+
 function App() {
-  const [inputEmail, setInputEmail ] = useState("")
-  const [inputPassword, setInputPassword ] = useState(1)
-
-  const handleChange = ({ target: { name, value } }) => {
-    if ( name === "Email" ) {
-      console.log(name, value);
-      setInputEmail(value)
-    }
-    if ( name === "Password" ) {
-      console.log(name, value);
-      setInputPassword(value)
-    }
-  }
-
-  const login = async () => {
-    try {
-      const response = await axios.post('http://localhost:3000/user/login',{
-        email: inputEmail,
-        password: inputPassword,
-      })
-      setInputEmail('');
-      setInputPassword(0);
-      console.log(response);
-    } catch(err) {
-      console.log('aqui oooo',err.message);
-    }
-  }
 
   return (
     <div>
-      <input 
-      type="text"
-      name='Email'
-      value={inputEmail}
-      onChange={handleChange} 
-      >
-      
-      </input>
-
-      <input 
-      type="number"
-      name='Password'
-      value={inputPassword}
-      onChange={handleChange}
-      >
-      
-      </input>
-      <button
-      type="button"
-      name='Password'
-      onClick={login}
-      >
-        login
-      </button>
+      <Routes>
+        <Route 
+        exact path="/" 
+        element={ <Navigate to="/login" replace/> } />
+        <Route 
+        path="/login"
+        element={ <Login/>}
+        />
+        </Routes>
     </div>
   );
 }
